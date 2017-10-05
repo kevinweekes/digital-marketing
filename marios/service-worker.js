be0e6f3a4e81d01dcb70c798dd7c35bcfd9a5faa
Kevin Ancil Weekes <kevinweekes@gmail.com>
2017-10-05 13:13:35 -0400

    Add icons and update web app manifest.

--- marios/service-worker.js
+++ marios/service-worker.js
@@ -1,4 +1,4 @@
-self.addEventListener("install", function(evt){
+self.addEventListener("install", function(evt) {
     evt.waitUntil(
         caches.open("marios-demo")
         .then(function(cache) {
@@ -6,22 +6,28 @@ self.addEventListener("install", function(evt){
                 [
                     "/",
                     "/index.html",
-                    "mapbox-gl.css",
-                    "mapbox-gl.js",
-                    "marios.json",
-                    "pizza-30.png"
+                    "icon-512x512.png",
+                    "icon-384x384.png",
+                    "icon-192x192.png",
+                    "icon-152x152.png",
+                    "icon-144x144.png",
+                    "icon-128x128.png",
+                    "icon-96x96.png",
+                    "icon-72x72.png"
                 ]
             );
         })
-        .catch (function(err){})
+        .catch(function(err) {})
     );
 });
 
-self.addEventListener("fetch", function(evt){
-    evt.respondWith(
-        caches.match(evt.request)
-        .then(function(response){
-            return response || fetch(evt.request);
-        })
-    );
+self.addEventListener("fetch", function(evt) {
+    if (evt.request.mode === 'navigate') {
+        evt.respondWith(
+            caches.match(evt.request)
+            .then(function(response) {
+                return response || fetch(evt.request);
+            })
+        );
+    }
 });
