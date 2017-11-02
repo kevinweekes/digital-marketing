cb0a1b02e92fe9fe3f6c3eb61a11b5a574330d40
Kevin Ancil Weekes <kevinweekes@gmail.com>
2017-11-02 17:38:49 -0400

    Update Service Worker.

--- marios-v2/service-worker.js
+++ marios-v2/service-worker.js
@@ -7,17 +7,17 @@ self.addEventListener("install", function(evt) {
                     "/",
                     "/index.html",
                     "/detabinator.js",
-                    "/side-nav.js",
-                    "/styles.css",
-                    "/map-styles.css",
-                    "/img/icon-512x512.png",
-                    "/img/icon-384x384.png",
-                    "/img/icon-192x192.png",
-                    "/img/icon-152x152.png",
-                    "/img/icon-144x144.png",
-                    "/img/icon-128x128.png",
-                    "/img/icon-96x96.png",
-                    "/img/icon-72x72.png"
+                    "./side-nav.js",
+                    "./styles.css",
+                    "./map-styles.css",
+                    "./img/icon-512x512.png",
+                    "./img/icon-384x384.png",
+                    "./img/icon-192x192.png",
+                    "./img/icon-152x152.png",
+                    "./img/icon-144x144.png",
+                    "./img/icon-128x128.png",
+                    "./img/icon-96x96.png",
+                    "./img/icon-72x72.png"
                 ]
             );
         })
